10bb37a05cc962a81b53e1d19f66e8aa4e90ae2a
dwaraki3 <dwarakigowda4@gmail.com>
2024-05-27 22:16:04 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "dwaraki gowda "
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Dwaraki"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "DG"
 )
 
 var ID ids.ID
